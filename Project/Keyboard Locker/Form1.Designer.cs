ce7148dff44333b1e464d19a68dac1863d946644
Villan-PC\Villan <i420f1@hotmail.com.tw>
2019-01-12 17:12:55 +0800

    InitInitial commit

---
+++ Project/Keyboard Locker/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Keyboard_Locker
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainButton = new System.Windows.Forms.Button();
+            this.WarningLabel = new System.Windows.Forms.Label();
+            this.LicenseLabel = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainButton
+            // 
+            this.MainButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainButton.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainButton.Location = new System.Drawing.Point(70, 20);
+            this.MainButton.Name = "MainButton";
+            this.MainButton.Size = new System.Drawing.Size(250, 100);
+            this.MainButton.TabIndex = 0;
+            this.MainButton.Text = "Lock";
+            this.MainButton.UseVisualStyleBackColor = true;
+            this.MainButton.Click += new System.EventHandler(this.MainButton_Click);
+            // 
+            // WarningLabel
+            // 
+            this.WarningLabel.AutoSize = true;
+            this.WarningLabel.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WarningLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.WarningLabel.Location = new System.Drawing.Point(40, 149);
+            this.WarningLabel.Name = "WarningLabel";
+            this.WarningLabel.Size = new System.Drawing.Size(320, 18);
+            this.WarningLabel.TabIndex = 1;
+            this.WarningLabel.Text = "※ (Ctrl + Alt + Delete) Still working.";
+            this.WarningLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LicenseLabel
+            // 
+            this.LicenseLabel.AutoSize = true;
+            this.LicenseLabel.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LicenseLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LicenseLabel.Location = new System.Drawing.Point(106, 192);
+            this.LicenseLabel.Name = "LicenseLabel";
+            this.LicenseLabel.Size = new System.Drawing.Size(168, 14);
+            this.LicenseLabel.TabIndex = 2;
+            this.LicenseLabel.Text = "Create by Villan © 2019";
+            this.LicenseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Keyboard Locker";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openWindowToolStripMenuItem,
+            this.lockToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(151, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // openWindowToolStripMenuItem
+            // 
+            this.openWindowToolStripMenuItem.Name = "openWindowToolStripMenuItem";
+            this.openWindowToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.openWindowToolStripMenuItem.Text = "Open Window";
+            this.openWindowToolStripMenuItem.Click += new System.EventHandler(this.openWindowToolStripMenuItem_Click);
+            // 
+            // lockToolStripMenuItem
+            // 
+            this.lockToolStripMenuItem.Name = "lockToolStripMenuItem";
+            this.lockToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.lockToolStripMenuItem.Text = "Lock";
+            this.lockToolStripMenuItem.Click += new System.EventHandler(this.lockToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(384, 215);
+            this.Controls.Add(this.LicenseLabel);
+            this.Controls.Add(this.WarningLabel);
+            this.Controls.Add(this.MainButton);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keyboard Locker";
+            this.TopMost = true;
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button MainButton;
+        private System.Windows.Forms.Label WarningLabel;
+        private System.Windows.Forms.Label LicenseLabel;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem openWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Project/Keyboard Locker/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Keyboard_Locker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetWindowsHookExW(int idHook, HookHandlerDelegate lpfn, IntPtr hmod, uint dwThreadID);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(String modulename);
+
+        public const int WM_KEYDOWN = 0x0100;
+        public const int WH_KEYBOARD_LL = 13;
+        public const int WM_SYSKEYDOWN = 0x0104;
+
+        public struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        public delegate int HookHandlerDelegate(int nCode, IntPtr wparam, ref KBDLLHOOKSTRUCT lparam);
+
+        private HookHandlerDelegate proc = null;
+        private readonly string LockString = "Lock";
+        private readonly string UnLockString = "UnLock";
+        private readonly Color RedColor = Color.FromKnownColor(KnownColor.OrangeRed);
+        private readonly Color GreenColor = Color.FromKnownColor(KnownColor.ForestGreen);
+        private bool m_IsLock = false;
+
+        private int HookCallback(int nCode, IntPtr wparam, ref KBDLLHOOKSTRUCT lparam)
+        {
+            return m_IsLock ? 1 : 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MainButton.BackColor = m_IsLock ? GreenColor : RedColor;
+
+            proc = new HookHandlerDelegate(HookCallback);
+            using (Process curPro = Process.GetCurrentProcess())
+            using (ProcessModule curMod = curPro.MainModule)
+            {
+                SetWindowsHookExW(WH_KEYBOARD_LL, proc, GetModuleHandle(curMod.ModuleName), 0);
+            }
+        }
+
+        private void MainButton_Click(object sender, EventArgs e)
+        {
+            m_IsLock = !m_IsLock;
+            MainButton.Text = m_IsLock ? UnLockString : LockString;
+            MainButton.BackColor = m_IsLock ? GreenColor : RedColor;
+
+            notifyIcon1.ShowBalloonTip(0, this.Text,
+                    string.Format("Keyboard now is 「{0}」", m_IsLock ? LockString : UnLockString),
+                    ToolTipIcon.Info);
+
+            System.GC.Collect();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.ShowInTaskbar = false;
+                this.WindowState = FormWindowState.Minimized;
+                notifyIcon1.Tag = string.Empty;
+                notifyIcon1.ShowBalloonTip(3000, this.Text,
+                    "Background running.",
+                    ToolTipIcon.Info);
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.ShowForm();
+        }
+
+        private void openWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowForm();
+        }
+
+        private void lockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainButton_Click(sender, e);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            lockToolStripMenuItem.Text = m_IsLock ? UnLockString : LockString;
+        }
+
+        private void ShowForm()
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.ShowInTaskbar = true;
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+    }
+}
